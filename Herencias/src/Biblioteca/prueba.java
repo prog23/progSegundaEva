842df91e74f71a0743976592626de6dd296105a4
prog23 <tamavilpet@gmail.com>
2019-01-31 13:24:16 +0100

    Ejercicio libros y revistas

--- Herencias/src/Biblioteca/prueba.java
+++ Herencias/src/Biblioteca/prueba.java
@@ -5,9 +5,16 @@ public class prueba {
 	public static void main(String[] args) {
 		
 		Libro libro= new Libro("123","El Quijote", 1605);
+		
 		Revista revista= new Revista("133","Fotogramas", 2019, 2011 );
+		
 		System.out.println(libro);
+		Ejemplar e = libro;		
+		Prestable p = libro;
+		
+		
 		System.out.println(revista);
+		Ejemplar r = revista;
 	}
 
 }
